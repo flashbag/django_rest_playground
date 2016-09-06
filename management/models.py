0f3c33fcfe5767ad2dbe059c861f6327dee93cc4
Oleg Kukil <flashbag.zvir@gmail.com>
2016-09-06 06:36:50 +0300

    models and migration updated

--- management/models.py
+++ management/models.py
@@ -1,11 +1,20 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import UserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
+
+class UserType(models.Model):
+
+   	name = models.CharField(_('name'), max_length=30, unique=True, blank=True, null=True,)
+
+class UserStatus(models.Model):
+
+    name = models.CharField(_('name'), max_length=30, unique=True, blank=True, null=True,)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -20,37 +29,22 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     date_joined = models.DateTimeField(_('Date Joined'), default=timezone.now)
 
-    user = models.OneToOneField( User, on_delete=models.CASCADE, primary_key=True, )
+    is_staff = models.BooleanField(_('staff status'), default=False,)
 
+    user_type = models.ForeignKey( UserType, null=True, )
+    user_status = models.ForeignKey( UserStatus, null=True, )
+
+    REQUIRED_FIELDS = ['email']
     USERNAME_FIELD = 'username'
+
+    objects = UserManager()
 
     class Meta(object):
         verbose_name = _('User')
         verbose_name_plural = _('Users')
         abstract = False
 
-    def get_full_name(self):
-        """
-        Returns email instead of the fullname for the user.
-        """
-        return email_to_name(self.email)
-
-    def get_short_name(self):
-        """
-        Returns the short name for the user.
-        This function works the same as `get_full_name` method.
-        It's just included for django built-in user comparability.
-        """
-        return self.get_full_name()
-
     def __str__(self):
-        return self.email
+        return self.username
 
 
-class UserType(models.Model):
-
-    user = models.OneToOneField( User, on_delete=models.CASCADE, primary_key=True, )
-
-class UserStatus(models.Model):
-
-    user = models.OneToOneField( User, on_delete=models.CASCADE, primary_key=True, )
