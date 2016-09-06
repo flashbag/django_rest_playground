3bff04f7bc2065d14886789303d57f5b4e4e8d38
Oleg Kukil <flashbag.zvir@gmail.com>
2016-09-06 07:18:27 +0300

    user views added

--- management/views.py
+++ management/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from rest_framework import mixins
+from rest_framework import generics
+
+from rest_framework import status
+from rest_framework import mixins
+
+from rest_framework import viewsets
+
+from management.models import User
+from management.serializers import UserSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+	"""
+	This viewset automatically provides `list` and `detail` actions.
+	"""
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
